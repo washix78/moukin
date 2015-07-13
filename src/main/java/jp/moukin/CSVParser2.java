0e9017adadd9c7bc55c03bdf78c061131e5923c3
max-waltham <max.k.saito@gmail.com>
2015-07-13 10:47:35 +0900

    modify comments

--- src/main/java/jp/moukin/CSVParser2.java
+++ src/main/java/jp/moukin/CSVParser2.java
@@ -3,10 +3,8 @@ package jp.moukin;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +15,7 @@ public class CSVParser2 {
 	private final String charset;
 
 	/**
-	 * RFC4180 CSVに従ったCSVのパースを行います。<br>
+	 * RFC4180 に従ったCSVのパースを行います。<br>
 	 * ただしヘッダーの有無、レコード内のカラム数が異なるなどは無視します。<br>
 	 * フォーマットに従わない場合などのエラー検知は不十分です。
 	 * 
